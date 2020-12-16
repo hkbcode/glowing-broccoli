f4a2e4ff5d7364742666bf7f79a6ff1defc1f693
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2020-12-16 13:48:03 +0530

    :zap: initial commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import { Button } from 'antd'
+
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+
+    <Button type="primary">Antdesign</Button>
+  </Layout>
+)
+
+export default SecondPage
